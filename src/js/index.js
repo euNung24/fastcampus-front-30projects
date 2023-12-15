75a234a1697e9f7cdc3463d1b42c253aee5c557e
eunung <eunung@gmail.com>
2023-12-15 22:38:23 +0900

    feat: init commit
    프로젝트 초기 설정
    - 디렉터리
    - webpack

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../css/style.css';
+console.log('hi');
+let a = 1;
+
+
+let b = 2;
+
+
+console.log(a+  b)
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CSSMinimizerPlugin = require('css-minimizer-webpack-plugin');
+module.exports = (env) => {
+    const isDevMode = env.mode === 'development';
+    const devServer = {
+        static: { directory: path.join(__dirname, "dist") },
+        watchFiles: './index.html',
+    };
+
+    return {
+        mode: isDevMode ? "development" : "production",
+        devtool: isDevMode ? "source-map" : "inline-source-map",
+        resolve: {
+            extensions: [".tsx",".ts", ".jsx", ".js"]
+        },
+        entry: "./src/js/index",
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader, 'css-loader'
+                    ]
+                }
+            ]
+        },
+        output: {
+            filename: "bundle.[chunkhash].js",
+            path: path.resolve(__dirname, './dist'),
+            // publicPath: "/",
+            clean: true
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./index.html",
+                inject: 'body',
+                favicon: "./favicon.png"
+            }),
+            new MiniCssExtractPlugin({
+                filename: "./style.css"
+            })
+        ],
+        optimization: {
+            minimizer: [
+                new TerserWebpackPlugin(),
+                new CSSMinimizerPlugin()
+            ]
+        },
+        ...isDevMode ? {devServer} : {}
+    }
+
+}
