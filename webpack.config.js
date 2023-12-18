4d4406930e5dcbd2ee478b297011402a45254a76
eunung <eunung@gmail.com>
2023-12-19 05:22:38 +0900

    feat: init commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env) => {
+  const isDevMode = env.mode === "development";
+  const devServer= {
+    watchFiles: 'index.html'
+  }
+
+  return {
+    mode: isDevMode ? "development" : "production",
+    devtool: isDevMode ? 'source-map' : "inline-source-map",
+    resolve: {
+      extensions: [".js"],
+    },
+    entry: "./src/js/index",
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.jpeg$/,
+          type:'asset/inline'
+        }
+      ],
+    },
+    output: {
+      filename: "bundle.[chunkhash].js",
+      path: path.resolve(__dirname, "./dist"),
+      assetModuleFilename: 'images/[hash][ext][query]',
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+        inject: "body",
+        favicon: "./favicon.png"
+      }),
+      new MiniCssExtractPlugin({ filename: "style.css" }),
+    ],
+    optimization: {
+      minimizer: [
+        new TerserPlugin(),
+        new CssMinimizerPlugin()
+      ]
+    },
+    ...isDevMode ? {devServer}: {}
+  }
+}
