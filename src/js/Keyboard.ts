0b1aef3f5991e81b2e72d92bb275dbd0b6689613
eunung <eunung@gmail.com>
2023-12-18 21:35:54 +0900

    feat: keyboard 이벤트 추가 및 한글 입력 처리 추가

--- src/js/Keyboard.ts
+++ src/js/Keyboard.ts
@@ -2,6 +2,10 @@ export default class Keyboard {
   $container: HTMLDivElement;
   $switchThemeEl: HTMLInputElement;
   $fontSelectEl: HTMLSelectElement;
+  $keyboardEl: HTMLDivElement;
+  $inputEl: HTMLInputElement;
+  $errorEl: HTMLParagraphElement;
+  koreanRegExp = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
   constructor() {
     this.setElements();
@@ -12,11 +16,17 @@ export default class Keyboard {
     this.$container = document.querySelector(".container");
     this.$switchThemeEl = this.$container.querySelector("[name='theme']");
     this.$fontSelectEl = this.$container.querySelector("[name='font']");
+    this.$keyboardEl = this.$container.querySelector(".keyboardBox");
+    this.$inputEl = this.$container.querySelector("[name=value]");
+    this.$errorEl = this.$container.querySelector(".message");
   }
 
   setEvents() {
     this.$switchThemeEl.addEventListener("change", this.onChangeTheme);
     this.$fontSelectEl.addEventListener("change", this.onChangeFont);
+    document.addEventListener("keydown", this.onKeyDown.bind(this));
+    document.addEventListener("keyup", this.onKeyUp.bind(this));
+    this.$inputEl.addEventListener("input", this.onChangeInput.bind(this));
   }
 
   onChangeTheme(e: Event) {
@@ -28,5 +38,26 @@ export default class Keyboard {
 
   onChangeFont(e: Event) {
     document.body.style.fontFamily = (e.target as HTMLSelectElement).value;
+  }
+
+  onKeyDown(e: KeyboardEvent) {
+    const value = e.key.toUpperCase();
+    this.$keyboardEl
+      .querySelector(`[data-key=${value}]`)
+      ?.closest(".keyBox")
+      .classList.add("active");
+    this.$errorEl.classList.toggle("error", this.koreanRegExp.test(value));
+  }
+
+  onKeyUp(e: KeyboardEvent) {
+    this.$keyboardEl
+      .querySelector(`[data-key=${e.key.toUpperCase()}]`)
+      ?.closest(".keyBox")
+      .classList.remove("active");
+  }
+
+  onChangeInput(e: Event) {
+    const el = e.target as HTMLInputElement;
+    el.value = el.value.replace(this.koreanRegExp, "");
   }
 }
