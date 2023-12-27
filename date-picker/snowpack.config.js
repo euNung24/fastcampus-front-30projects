e28881a56dd874a3eeaf639dd5ae74a1c998505c
eunung <pandamon24@naver.com>
2023-12-28 04:59:05 +0900

    feat: date-picker init commit

---
+++ date-picker/snowpack.config.js
@@ -0,0 +1,13 @@
+module.exports ={
+    mount:{
+        public: {url: '/', static: true},
+        src:{url:'/dist'}
+    },
+    optimize:{
+        minify:true
+    },
+    plugins:[
+        '@snowpack/plugin-sass'
+    ]
+}
+
