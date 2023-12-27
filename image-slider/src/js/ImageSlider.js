503eeb8c19e09c98f98273d6c602494eadfe17be
eunung <pandamon24@naver.com>
2023-12-28 00:34:24 +0900

    feat: autoPlay 기능 추가

--- image-slider/src/js/ImageSlider.js
+++ image-slider/src/js/ImageSlider.js
@@ -1,18 +1,30 @@
 export default class ImageSlider {
   $containerEl;
+
   $sliderEl;
+
   $prevBtn;
+
   $nextBtn;
+
   $indicatorEl;
+
   $length;
+
   $crtSlideIdx = 0;
+
   $slideWidth;
+
+  $intervalId;
+
+  $autoPlay = true;
 
   constructor() {
     this.setElements();
     this.initSetting();
     this.createIndicator();
     this.setEvents();
+    this.setAutoPlay();
   }
 
   setElements() {
@@ -21,11 +33,13 @@ export default class ImageSlider {
     this.$prevBtn = this.$containerEl.querySelector('.btn-prev');
     this.$nextBtn = this.$containerEl.querySelector('.btn-next');
     this.$indicatorEl = this.$containerEl.querySelector('.indicator-box');
+    this.$autoPlayEl = this.$containerEl.querySelector('.btn-control-box');
   }
+
   initSetting() {
     this.$length = this.$sliderEl.querySelectorAll('li').length;
     this.$slideWidth = this.$containerEl.clientWidth;
-    this.$sliderEl.style.width = this.$slideWidth * this.$length + 'px';
+    this.$sliderEl.style.width = `${this.$slideWidth * this.$length}px`;
   }
 
   setEvents() {
@@ -35,15 +49,20 @@ export default class ImageSlider {
       'click',
       this.handleIndicator.bind(this),
     );
+    this.$autoPlayEl.addEventListener('click', this.handleAutoPlay.bind(this));
   }
 
   moveSlide() {
     this.$sliderEl.style.left = `-${this.$crtSlideIdx * this.$slideWidth}px`;
     this.setIndicator();
+    if (this.$intervalId) {
+      clearInterval(this.$intervalId);
+    }
+    this.setAutoPlay();
   }
 
   handlePrevBtn() {
-    this.$crtSlideIdx--;
+    this.$crtSlideIdx -= 1;
     if (this.$crtSlideIdx < 0) {
       this.$crtSlideIdx = this.$length - 1;
     }
@@ -51,7 +70,7 @@ export default class ImageSlider {
   }
 
   handleNextBtn() {
-    this.$crtSlideIdx++;
+    this.$crtSlideIdx += 1;
     if (this.$crtSlideIdx > this.$length - 1) {
       this.$crtSlideIdx = 0;
     }
@@ -60,10 +79,12 @@ export default class ImageSlider {
 
   createIndicator() {
     const fragment = document.createDocumentFragment();
-    for (let i = 0; i < this.$length; i++) {
+    for (let i = 0; i < this.$length; i += 1) {
       const li = document.createElement('li');
       li.dataset.index = i.toString();
-      this.$crtSlideIdx === i && li.classList.add('active');
+      if (this.$crtSlideIdx === i) {
+        li.classList.add('active');
+      }
       fragment.appendChild(li);
     }
     this.$indicatorEl.appendChild(fragment);
@@ -82,5 +103,26 @@ export default class ImageSlider {
       10,
     );
     this.moveSlide();
+  }
+
+  setAutoPlay() {
+    if (this.$autoPlay) {
+      this.$intervalId = setInterval(this.handleNextBtn.bind(this), 2500);
+      this.$autoPlayEl.classList.add('play');
+      this.$autoPlayEl.classList.remove('pause');
+    } else {
+      clearInterval(this.$intervalId);
+      this.$autoPlayEl.classList.add('pause');
+      this.$autoPlayEl.classList.remove('play');
+    }
+  }
+
+  handleAutoPlay(e) {
+    if (e.target.closest('button').dataset.status === 'play') {
+      this.$autoPlay = true;
+    } else if (e.target.closest('button').dataset.status === 'pause') {
+      this.$autoPlay = false;
+    }
+    this.setAutoPlay();
   }
 }
