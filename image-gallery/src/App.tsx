f88af7da5a203c93cd1bb009dcdf9e6651372989
eunung <pandamon24@naver.com>
2024-01-05 03:20:30 +0900

    feat: Image Gallery - 퍼블리싱

--- image-gallery/src/App.tsx
+++ image-gallery/src/App.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="container">
+      <section className="init-box">
+        <p className="text-center">
+          이미지가 없습니다. <br />
+          이미지를 추가해주세요.
+        </p>
+        <button className="plus-btn">+</button>
+      </section>
+    </div>
+  );
 }
 
 export default App;
