546416b5b47803484fc1b287e9fef2a0509c4b2f
eunung <pandamon24@naver.com>
2024-01-05 01:49:47 +0900

    feat: BMI 계산기 - 로직 구현

--- bmi-calculator/main.js
+++ bmi-calculator/main.js
@@ -1,5 +1,7 @@
 function BMICalculator() {
   const formEl = document.querySelector(".bmi-calculator-wrapper form");
+  const resultEl = formEl.querySelector(".result");
+  const resultStateEl = resultEl.querySelector(".state span");
 
   const onSubmit = (e) => {
     const { target } = e;
@@ -11,8 +13,30 @@ function BMICalculator() {
       alert("적절한 값이 아닙니다.");
       return;
     }
+    let bmiValue = parseFloat((weight / (height * height)).toFixed(2));
+    let state = "정상";
+    let isCommonValue = true;
+    if (bmiValue < 18.5) {
+      state = "저체중";
+      isCommonValue = false;
+    } else if (bmiValue >= 23) {
+      state = "과체중";
+      isCommonValue = false;
+    }
+
+    resultEl.querySelector(".bmi-value span").textContent = bmiValue.toString();
+    resultEl.querySelector("meter").value = bmiValue;
+    resultStateEl.textContent = state;
+    isCommonValue && resultStateEl.classList.add("caution");
+    resultEl.classList.add("show");
   };
+
+  const onReset = (e) => {
+    resultEl.classList.remove("show");
+  };
+
   formEl.addEventListener("submit", onSubmit);
+  formEl.addEventListener("reset", onReset);
 }
 
 BMICalculator();
