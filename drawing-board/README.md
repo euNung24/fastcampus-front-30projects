80ae44466d97e37c2384a530f3110939f8a11289
eunung <pandamon24@naver.com>
2024-01-06 02:06:06 +0900

    feat: Drawing Board - 브러시 기능 적용

---
+++ drawing-board/README.md
@@ -0,0 +1,18 @@
+### 브러쉬
+
+- 브러시 선택
+  - [x] Brush Tool Active
+  - [x] Brush Panel Active
+- 마우스 Down
+  - [x] 브러시 그리기 시작
+- 마우스 Up
+  - [x] 브러시 그리기 종료
+- 마우스 Out
+  - [x] 브러시 그리기 종료
+- 마우스 Move
+  - [x] 마우스 Down 상태일 시 이어그리기
+  
+- 기본 기능
+  - [x] 크기 조정 (default: 10)
+  - [x] 색상 조정 (default: #000)
+
--- drawing-board/src/js/index.js
+++ drawing-board/src/js/index.js
@@ -1,1 +1,106 @@
-console.log("Hello");
+class DrawingBoard {
+  BRUSH = {
+    color: "#000",
+    size: 10,
+  };
+
+  isMouseDown = false;
+
+  constructor() {
+    this.setElements();
+    this.init();
+    this.setEvents();
+  }
+
+  setElements() {
+    this.wrapperEl = document.querySelector(".drawing-board-wrapper");
+    this.drawingBoardEl = this.wrapperEl.querySelector(".drawing-board");
+    this.toolbarEl = this.drawingBoardEl.querySelector(".toolbar");
+    this.brushEl = this.toolbarEl.querySelector(".brush");
+    this.brushColorInputEl = this.toolbarEl.querySelector(".color input");
+    this.canvasEl = this.drawingBoardEl.querySelector("canvas");
+    this.brushPanelEl = this.wrapperEl.querySelector(".brush-panel");
+    this.brushSizeInputEl = this.brushPanelEl.querySelector("input");
+    this.brushInfoEl = this.brushPanelEl.querySelector(".circle");
+  }
+
+  init() {
+    // 브러쉬 active
+    this.brushEl.classList.add("active");
+    this.brushPanelEl.classList.remove("hide");
+    this.canvasEl.style.cursor = "crosshair";
+    // canvas 설정
+    this.ctx = this.canvasEl.getContext("2d");
+    const { width, height } = this.canvasEl.getBoundingClientRect();
+    this.canvasEl.width = width;
+    this.canvasEl.height = height;
+    // 브러시 기본 세팅
+    this.brushSizeInputEl.value = this.BRUSH.size;
+    this.brushInfoEl.style.width = this.BRUSH.size + "px";
+    this.brushInfoEl.style.height = this.BRUSH.size + "px";
+    this.brushInfoEl.style.backgroundColor = this.BRUSH.color;
+  }
+
+  setEvents() {
+    this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+    this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+    this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
+    this.canvasEl.addEventListener("mouseup", this.onFinishBrush.bind(this));
+    this.canvasEl.addEventListener("mouseout", this.onFinishBrush.bind(this));
+    this.brushSizeInputEl.addEventListener(
+      "change",
+      this.onChangeBrushSize.bind(this)
+    );
+    this.brushColorInputEl.addEventListener(
+      "input",
+      this.onChangeBrushColor.bind(this)
+    );
+  }
+
+  onClickBrush(e) {
+    e.currentTarget.classList.add("active");
+    this.brushPanelEl.classList.remove("hide");
+  }
+
+  getMousePosition(e) {
+    const { left, top } = this.canvasEl.getBoundingClientRect();
+    return {
+      x: e.clientX - left,
+      y: e.clientY - top,
+    };
+  }
+
+  onMouseDown(e) {
+    this.isMouseDown = true;
+    const { x, y } = this.getMousePosition(e);
+    this.ctx.beginPath();
+    this.ctx.lineCap = "round";
+    this.ctx.lineWidth = this.BRUSH.size;
+    this.ctx.strokeStyle = this.BRUSH.color;
+    this.ctx.moveTo(x, y);
+  }
+
+  onMouseMove(e) {
+    if (!this.isMouseDown) return;
+    const { x, y } = this.getMousePosition(e);
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+  }
+
+  onFinishBrush() {
+    this.isMouseDown = false;
+  }
+
+  onChangeBrushSize(e) {
+    this.BRUSH.size = +e.target.value;
+    this.brushInfoEl.style.width = this.BRUSH.size + "px";
+    this.brushInfoEl.style.height = this.BRUSH.size + "px";
+  }
+
+  onChangeBrushColor(e) {
+    this.BRUSH.color = e.target.value;
+    this.brushInfoEl.style.backgroundColor = this.BRUSH.color;
+  }
+}
+
+new DrawingBoard();
